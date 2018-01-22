28c2895abab61ada43802f3cf4076815a6d01b04
xiaoyuting <479072483@qq.com>
2018-01-22 21:04:34 +0800

    upload

--- ProjectTemplate/InfoGroup/GMinfoVC.m
+++ ProjectTemplate/InfoGroup/GMinfoVC.m
@@ -28,7 +28,7 @@
 }
 -(void)rightItemClick:(id)sender{
     [[GMpopView popView] popShearComtentItem:nil];
-     [[GMpopView popView] popShearComtentItem:nil];
+    
 }
 -(void)select:(UIButton *)sender{
     if (sender.tag == ShareTypeWeiBo) {
